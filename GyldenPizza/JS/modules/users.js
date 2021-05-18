79eb0dcc6d8abac5fc1e7b8334a190cca11e64c1
sigve0809 <sigve.b.langva@hotmail.com>
2021-05-18 15:59:11 +0200

    users

---
+++ GyldenPizza/JS/modules/users.js
@@ -0,0 +1,27 @@
+const Users = (function(){
+    const usersArray = [
+        {name: "", email: "", phone: "", location: "", picture: "", workplace: ""},
+        {name: "", email: "", phone: "", location: "", picture: "", workplace: ""},
+        {name: "", email: "", phone: "", location: "", picture: "", workplace: ""},
+        {name: "", email: "", phone: "", location: "", picture: "", workplace: ""},
+        {name: "", email: "", phone: "", location: "", picture: "", workplace: ""},
+        {name: "", email: "", phone: "", location: "", picture: "", workplace: ""},
+    ];
+
+    const getAll = () => warriorTypeArray;
+
+    const getByName = ( name ) => {
+        return usersArray.filter( userObject => userObject.name === name);
+    }
+    const getByEmail = (email) => {
+        return usersArray.filter(userObject => userObject.email === email);
+    }
+    const getByPhone = (phone) => {
+        return usersArray.filter(userObject => userObject.phone === phone);
+    }
+    const getByWorkplace = (workplace) => {
+        return usersArray.filter(userObject => userObject.workplace === workplace);
+    }
+    return {getAll, getByName, getByEmail, getByPhone, getByWorkplace};
+ }());
+ export default WarriorTypeModule;
