f53f669440522593bc87427be0dcbcdfca05bfe0
Sanosh Senthilkumar <64579703+SanoshSenthilkumar@users.noreply.github.com>
2021-05-19 19:24:20 +0200

    Updated and moved pictures and fixed menu js,css and html

--- GyldenPizza/JS/Meny.js
+++ GyldenPizza/JS/Meny.js
@@ -1,13 +1,23 @@
 import pizzaMenu from './modules/Inventory.js';
 
-let menuPrintDiv = document.getElementById("print-menu");
-let nameDiv = document.createElement("h2");
+let menuPrintPizzaDiv = document.getElementById("print-menu-pizza");
+let menuPrintDrinkDiv = document.getElementById("print-menu-drinks");
 
+function printMenu(){
+    printMenuPizza();
+    printMenuDrinks();
+}
 
-let printMenu = () => {
-    pizzaMenu.getAll().forEach(pizzaObject => {
-        menuPrintDiv.innerHTML += `<img src="resources/images/${pizzaObject.img}><h2>${pizzaObject.name}</h2>`;
+let printMenuPizza = () => {
+    pizzaMenu.getAllPizza().forEach(pizzaObject => {
+        menuPrintPizzaDiv.innerHTML += `<div id="element"><img id="pic-pizza" src="resources/images-pizza/${pizzaObject.img}"><h2>${pizzaObject.name}</h2></div>`;
     });
+}
+
+let printMenuDrinks = () => {
+    pizzaMenu.getAllDrinks().forEach(drinkObject => {
+        menuPrintDrinkDiv.innerHTML += `<img id="pic-soda" src="resources/images-soda/${drinkObject.img}"><h2>${drinkObject.name}</h2>`;
+    })
 }
 
 printMenu();
