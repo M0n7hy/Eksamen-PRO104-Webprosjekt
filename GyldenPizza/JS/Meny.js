d08c58fc39116f52284ae7e05a2f401913f09af1
Sanosh Senthilkumar <64579703+SanoshSenthilkumar@users.noreply.github.com>
2021-05-19 19:49:40 +0200

    Fixed placement and added h1.
    Relocated price and name over picture. Added center align and flex-box.  Added h1="Meny".

--- GyldenPizza/JS/Meny.js
+++ GyldenPizza/JS/Meny.js
@@ -10,13 +10,21 @@ function printMenu(){
 
 let printMenuPizza = () => {
     pizzaMenu.getAllPizza().forEach(pizzaObject => {
-        menuPrintPizzaDiv.innerHTML += `<div id="element"><img id="pic-pizza" src="resources/images-pizza/${pizzaObject.img}"><h2>${pizzaObject.name}</h2></div>`;
+        menuPrintPizzaDiv.innerHTML +=
+            `<div id="element">
+                <h2>${pizzaObject.name}</h2>
+                <h3>${pizzaObject.price}kr</h3>
+                <img id="pic-pizza" src="resources/images-pizza/${pizzaObject.img}">
+            </div>`;
     });
 }
 
 let printMenuDrinks = () => {
     pizzaMenu.getAllDrinks().forEach(drinkObject => {
-        menuPrintDrinkDiv.innerHTML += `<img id="pic-soda" src="resources/images-soda/${drinkObject.img}"><h2>${drinkObject.name}</h2>`;
+        menuPrintDrinkDiv.innerHTML +=
+            `<h2>${drinkObject.name}</h2>
+            <h3>${drinkObject.price}kr</h3>
+            <img id="pic-soda" src="resources/images-soda/${drinkObject.img}">`;
     })
 }
 
