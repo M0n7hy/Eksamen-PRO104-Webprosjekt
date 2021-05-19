4fc63b05f140dfb276747aa19cc9aab9870af119
Sanosh Senthilkumar <64579703+SanoshSenthilkumar@users.noreply.github.com>
2021-05-19 14:26:36 +0200

    Added Meny module
    Added Meny module and more folders

---
+++ GyldenPizza/JS/modules/pizza.js
@@ -0,0 +1,10 @@
+//Object for the menu, with info about name, price, profitt, allergies,
+let pizza = [
+    { id: 0, name:"Classic Margarita", size:"Small", price: 120, profitt:60, img: "images/man1.jpg", allergies: "Gulten, Milk"},
+    { id: 1, name:"Large Classic Margarita", size:"Large", price: 160, profitt:75, img:?, allergies:"Gluten, Milk"},
+    { id: 1, name:"Large Classic Margarita", size:"Large", price: 160, profitt:75, img:?, allergies:"Gluten, Milk"},
+    { id: 1, name:"Large Classic Margarita", size:"Large", price: 160, profitt:75, img:?, allergies:"Gluten, Milk"},
+    { id: 1, name:"Large Classic Margarita", size:"Large", price: 160, profitt:75, img:?, allergies:"Gluten, Milk"},
+    { id: 1, name:"Large Classic Margarita", size:"Large", price: 160, profitt:75, img:?, allergies:"Gluten, Milk"},
+    { id: 1, name:"Large Classic Margarita", size:"Large", price: 160, profitt:75, img:?, allergies:"Gluten, Milk"},
+    ];
