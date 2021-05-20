35de56eafbc90979d111fa244a86b9500e12996f
sigve0809 <sigve.b.langva@hotmail.com>
2021-05-21 01:22:35 +0200

    nav logout og quickfix

--- GyldenPizza/JS/modules/Login.js
+++ GyldenPizza/JS/modules/Login.js
@@ -1,9 +1,13 @@
 const UsersLogin = (function(){
     const usersLoginArray = [
-        {position: "admin", email: "admin", password: "admin"},
-        {position: "manager", email: "manager1", password: "manager"},
-        {position: "manager", email: "manager2", password: "manager"},
-        {position: "manager", email: "manager3", password: "manager"}
+        {position: "admin", email: "sigve", password: "sigve"},
+        {position: "manager", email: "sanosh", password: "sanosh"},
+        {position: "manager", email: "sondre", password: "sondre"},
+        {position: "manager", email: "kamil", password: "kamil"},
+        {position: "manager", email: "mathias", password: "mathias"},
+        {position: "manager", email: "rolando", password: "rolando"},
+        {position: "manager", email: "eivind", password: "eivind"}
+
     ];
     const getAll = () => usersLoginArray;
 
--- GyldenPizza/JS/modules/users.js
+++ GyldenPizza/JS/modules/users.js
@@ -1,11 +1,13 @@
 const Users = (function(){
     const usersArray = [
-        {name: "sigve", email: "admin", phone: "97890994", location: "norge", picture: "", workplace: "all"},
-        {name: "sanosh", email: "manager1", phone: "2344354", location: "norge", picture: "", workplace: "drammen"},
-        {name: "sondre", email: "manager2", phone: "97546848", location: "norge", picture: "", workplace: "alesund"},
-        {name: "", email: "", phone: "", location: "", picture: "", workplace: ""},
-        {name: "", email: "", phone: "", location: "", picture: "", workplace: ""},
-        {name: "", email: "", phone: "", location: "", picture: "", workplace: ""}
+        {name: "Sigve Berg Langva", email: "sigve", phone: "97890994", location: "norge", picture: "", workplace: "all"},
+        {name: "Sanosh", email: "sanosh", phone: "23443549", location: "norge", picture: "", workplace: "drammen"},
+        {name: "Sondre", email: "sondre", phone: "97546848", location: "norge", picture: "", workplace: "alesund"},
+        {name: "Kamil", email: "kamil", phone: "12433453", location: "norge", picture: "", workplace: "oslo"},
+        {name: "Mathias", email: "mathias", phone: "12345678", location: "norge", picture: "", workplace: "oslo"},
+        {name: "Rolando", email: "rolando", phone: "98765432", location: "norge", picture: "", workplace: "oslo"},
+        {name: "Eivind", email: "eivind", phone: "55556666", location: "norge", picture: "", workplace: "oslo"}
+
     ];
 
     const getAll = () => usersArray;
