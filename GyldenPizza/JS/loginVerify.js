4843a09cce5e0a2795d14ccff2a47e147043c096
sigve0809 <sigve.b.langva@hotmail.com>
2021-05-21 00:01:24 +0200

    logout site

--- GyldenPizza/JS/loginVerify.js
+++ GyldenPizza/JS/loginVerify.js
@@ -1,3 +1,10 @@
-if(localStorage.getItem("level")!=="admin" || localStorage.getItem("level")==!"manager"){
+let loggedIn = false;
+if(localStorage.getItem("level")=="admin" || localStorage.getItem("level")=="manager"){
+    loggedIn = true;
+}
+if(!loggedIn){
     location.href = "login.html";
+}
+if (window.location.pathname !=="/GyldenPizza/logout.html") {
+    localStorage.setItem("page", window.location.pathname);
 }
--- GyldenPizza/JS/logout.js
+++ GyldenPizza/JS/logout.js
@@ -1,6 +1,13 @@
+let personName = document.getElementById("name-div");
+
 function logout(){
     localStorage.removeItem("level");
     localStorage.removeItem("name");
     localStorage.removeItem("workplace");
+    localStorage.removeItem("page");
     location.href = "login.html";
 }
+function cancel(){
+    location.href = localStorage.getItem("page");
+}
+personName.innerHTML = localStorage.getItem("name");
