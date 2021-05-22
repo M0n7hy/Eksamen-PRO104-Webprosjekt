ff7a4c099f23d19c2950776b36438a10408e4923
Sanosh Senthilkumar <64579703+SanoshSenthilkumar@users.noreply.github.com>
2021-05-22 17:45:05 +0200

    Edits on inventory.js

--- GyldenPizza/JS/modules/Inventory.js
+++ GyldenPizza/JS/modules/Inventory.js
@@ -2,15 +2,15 @@ const pizzaMenu = (function() {
 
     //Object for the menu, with info about name, size, price, profitt, images and allergies
     const pizzaArray = [
-        { id: 0, name:"Classic Margarita", price: 120, profit:60, img:"Margarita.png", allergies: "Gulten, Milk"},
-        { id: 1, name:"Pepperoni Pizza", price: 140, profit:70, img:"Pepperoni.png", allergies:"Gluten, Milk"},
-        { id: 2, name:"Kebab Pizza", price: 150, profit:75, img:"Kebab.png", allergies:"Gluten, Milk"},
-        { id: 3, name:"Taco Pizza", price:150, profit:70, img:"Tacopizza.png", allergies:"Gluten, Milk"},
-        { id: 4, name:"Pizza Parma", price:135, profit:80, img:"Parma.png", allergies:"Gluten, Milk"},
-        { id: 5, name:"Pizza Pollo", price:145, profit:70, img:"Pollo.png", allergies:"Gluten, Milk"},
-        { id: 6, name:"Beef Pizza", price:155, profit:65, img:"Biffpizza.png", allergies:"Gluten, Milk"},
-        { id: 7, name:"Cheddar Dream", price:130, profit:85, img:"Cheddar.png", allergies:"Gluten, Milk"},
-        { id: 8, name:"The OG", price:145, profit:70, img:"Klassikeren.png", allergies:"Gluten, Milk"}
+        { id: 0, name:"Pepperoni Pizza", price: 140, profit:70, img:"Pepperoni.png", allergies:"Gluten, Milk"},
+        { id: 1, name:"Kebab Pizza", price: 150, profit:75, img:"Kebab.png", allergies:"Gluten, Milk"},
+        { id: 2, name:"Taco Pizza", price:150, profit:70, img:"Tacopizza.png", allergies:"Gluten, Milk"},
+        { id: 3, name:"The OG", price:145, profit:70, img:"Klassikeren.png", allergies:"Gluten, Milk"},
+        { id: 4, name:"Beef Pizza", price:155, profit:65, img:"Biffpizza.png", allergies:"Gluten, Milk"},
+        { id: 5, name:"Cheddar Dream", price:130, profit:85, img:"Cheddar.png", allergies:"Gluten, Milk"},
+        { id: 6, name:"Classic Margarita", price: 120, profit:60, img:"Margarita.png", allergies: "Gulten, Milk"},
+        { id: 7, name:"Pizza Pollo", price:145, profit:70, img:"Pollo.png", allergies:"Gluten, Milk"},
+        { id: 8, name:"Pizza Parma", price:135, profit:80, img:"Parma.png", allergies:"Gluten, Milk"}
     ];
 
     const drinkArray = [
