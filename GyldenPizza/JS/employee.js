82ecbe715d1a6abbaea0b5ac8193989dedc65bf0
sigve0809 <sigve.b.langva@hotmail.com>
2021-05-24 07:27:58 +0200

    employee

--- GyldenPizza/JS/employee.js
+++ GyldenPizza/JS/employee.js
@@ -8,20 +8,33 @@ let editDiv = document.getElementById("edit-users");
 let addDiv = document.getElementById("add-users");
 let removeDiv = document.getElementById("remove-users");
 let addUserPopup = document.getElementById("add-user-popup");
-
+let xAdd = document.getElementById("x-add");
+let submit = document.getElementById("submit");
+let editContainer = document.getElementById("edit");
+let headerEdit = document.getElementById("name-edit-top");
+let editName = document.getElementById("edit-name");
+let editEmail = document.getElementById("edit-email");
+let editPhone = document.getElementById("edit-phone");
+let editPicture = document.getElementById("edit-picture");
+let editLocation = document.getElementById("edit-location");
+let editWorkplace = document.getElementById("edit-workplace");
+let editInfo = document.getElementById("edit-info");
+let submitEdit = document.getElementById("submit-edit");
+let xEdit = document.getElementById("edit-x");
 let edit = false;
 let add = false;
 let remove = false;
 
 let printEmployees = () => {
     let count = 0;
-  
+
+  usersOutput.innerHTML = "";
     Users.getAll().forEach(user => {
         usersOutput.innerHTML +=
             `
             <div id="${user.id}" class="users-div span-1-of-4" onclick="getPerson(${user.id})">
                 <div class="name">${user.name}</div>
-                <img style="width: 100%" src="${user.picture}" alt="">
+                <img class="profile-pic" src="${user.picture}" alt="">
                 <div class="email info">Email: ${user.email}</div>
                 <div class="phone info">Tlf: ${user.phone}</div>
                 <div class="location info">Hjem: ${user.location}</div>
@@ -41,7 +54,7 @@ search.addEventListener('input', (event) =>{
         usersOutput.innerHTML +=`
             <div id="${user.id}" class="users-div span-1-of-4" onclick="getPerson(${user.id})">
                 <div class="name">${user.name}</div>
-                <img style="width: 100%" src="${user.picture}" alt="">
+                <img class="profile-pic" src="${user.picture}" alt="">
                 <div class="email info">Email: ${user.email}</div>
                 <div class="phone info">Tlf: ${user.phone}</div>
                 <div class="location info">Hjem: ${user.location}</div>
@@ -51,25 +64,88 @@ search.addEventListener('input', (event) =>{
     });
 })
 window.getPerson = (id)=> {
-    let objectId = id.id;
-    console.log(objectId);
-    console.log(Users.getById(objectId));
-    localStorage.setItem("id", objectId);
-    Users.getById(objectId).forEach(user => {
-        pickedUser.innerHTML =`
-            <div id="image" onclick="closeWindow()">
-                <img src="/GyldenPizza/resources/images-employee/x-mark-4-24.png" alt="x">
-            </div>
-            <div class="name-popup popup-item">${user.name}</div>
-            <div class="email-popup popup-item">email: ${user.email}</div>
-            <div class="phone-popup popup-item">phone: ${user.phone}</div>
-            <div class="location-popup popup-item">location: ${user.location}</div>
-            <div class="workplace-popup popup-item">workplace: ${user.workplace}</div>
-            <div class="info-popup popup-item">${user.info}</div>
-        `;
-        pickedUser.style.display = "block";
-        backgroundPicked.style.display = "block"
-    });
+    let test;
+    console.log(id);
+    if(remove){
+        Users.removeUser(id);
+        printEmployees();
+    }
+    if (edit) {
+        editContainer.style.display = "block";
+        backgroundPicked.style.display = "block";
+    }
+    else{
+        Users.getById(id).forEach(user => {
+            pickedUser.innerHTML =`
+                <div id="image" onclick="closeWindow()">
+                    <img src="/GyldenPizza/resources/images-employee/x-mark-4-24.png" alt="x">
+                </div>
+                <div class="name-popup popup-item">${user.name}</div>
+                <div class="email-popup popup-item">email: ${user.email}</div>
+                <div class="phone-popup popup-item">phone: ${user.phone}</div>
+                <div class="location-popup popup-item">location: ${user.location}</div>
+                <div class="workplace-popup popup-item">workplace: ${user.workplace}</div>
+                <div class="info-popup popup-item">${user.info}</div>
+            `;
+            pickedUser.style.display = "block";
+            backgroundPicked.style.display = "block"
+        });
+    }
+    function editItem(){
+        editName.style.fontWeight = "normal";
+        editEmail.style.fontWeight = "normal";
+        editPhone.style.fontWeight = "normal";
+        editPicture.style.fontWeight = "normal";
+        editLocation.style.fontWeight = "normal";
+        editWorkplace.style.fontWeight = "normal";
+        editInfo.style.fontWeight = "normal";
+
+        switch(this.id){
+            case "edit-name":
+                test = "name";
+                editName.style.fontWeight = "bold";
+                break;
+            case "edit-email":
+                editEmail.style.fontWeight = "bold";
+                test = "email"
+                break;
+            case "edit-phone":
+                test = "phone";
+                editPhone.style.fontWeight = "bold";
+                break;
+            case "edit-picture":
+                test = "picture";
+                editPicture.style.fontWeight = "bold";
+                break;
+            case "edit-location":
+                test = "location";
+                editLocation.style.fontWeight = "bold";
+                break;
+            case "edit-workplace":
+                test = "workplace";
+                editWorkplace.style.fontWeight = "bold";
+                break;
+            case "edit-info":
+                test = "info";
+                editInfo.style.fontWeight = "bold";
+                break;
+        }
+        
+    }
+    function runEdit(){
+        Users.editUser(id, test, document.getElementById("new-edit").value);
+        printEmployees();
+    }
+    
+    editName.onclick = editItem;
+    editEmail.onclick = editItem;
+    editPhone.onclick = editItem;
+    editPicture.onclick = editItem;
+    editLocation.onclick = editItem;
+    editWorkplace.onclick = editItem;
+    editInfo.onclick = editItem;
+    submitEdit.onclick = runEdit;
+
 };
 printEmployees();
 
@@ -77,21 +153,20 @@ window.closeWindow = ()=>{
     pickedUser.style.display = "none";
     backgroundPicked.style.display = "none"
 }
-editDiv.onclick = interact;
-addDiv.onclick = interact;
-removeDiv.onclick = interact;
+
 function interact() {
     switch(this.id){
         case "edit-users":
             if(!edit){
                 edit = true;
-                editDiv.innerHTML = "stop editing";
-            }
+                remove = false;
+                editDiv.innerHTML = `<img src="/GyldenPizza/resources/images-employee/edit.png" alt="">`;
+                removeDiv.innerHTML = `<img src="/GyldenPizza/resources/images-employee/disapprove-64.png" alt="">`;
 
-            else 
-            {
+            }
+            else {
                 edit = false;
-                editDiv.innerHTML = "edit";
+                editDiv.innerHTML = `<img src="/GyldenPizza/resources/images-employee/edit-64.png" alt="">`;
             }
             console.log(edit);
             break;
@@ -106,12 +181,14 @@ function interact() {
         case "remove-users":
             if(!remove){
                 remove = true;
-                removeDiv.innerHTML = "stop editing";
+                edit = false;
+                editDiv.innerHTML = `<img src="/GyldenPizza/resources/images-employee/edit-64.png" alt="">`;
+                removeDiv.innerHTML = ` <img src="/GyldenPizza/resources/images-employee/remove.png" alt="">`;
             }
             else 
             {
                 remove = false;
-                removeDiv.innerHTML = "edit";
+                removeDiv.innerHTML = `<img src="/GyldenPizza/resources/images-employee/disapprove-64.png" alt="">`;
             }
             console.log(remove);
             break;
@@ -122,3 +199,52 @@ let addUser = ()=>{
     addUserPopup.style.display = "block";
     backgroundPicked.style.display = "block";
 }
+
+let closeAddWindow = ()=>{
+    addUserPopup.style.display = "none";
+    backgroundPicked.style.display = "none";
+}
+let closeEditWindow = ()=>{
+    editContainer.style.display = "none";
+    backgroundPicked.style.display = "none";
+}
+
+let addUserToArray = () =>{
+    let userId = Users.length+1;
+    let name = document.getElementById("add-name").value;
+    let email = document.getElementById("add-email").value;
+    let phone = document.getElementById("add-phone").value;
+    let location = document.getElementById("add-location").value;
+    let picture = document.getElementById("add-picture").value;
+    let workplace = document.getElementById("add-workplace").value;
+    let info = document.getElementById("add-info").value;
+    if(picture ==""){
+        picture = "/GyldenPizza/resources/images-users/profile-pic.jpg";
+    }
+    if(name == "" || email == "" || phone == "" || location =="" || workplace =="" || info ==""){
+        alert("All info required");
+    }
+    else{
+ 
+        let user = {
+            id: userId,
+            name: name,
+            email: email,
+            phone: phone,
+            location: location,
+            picture: picture,
+            workplace: workplace,
+            info: info
+        }
+        Users.addUser(user);
+        usersOutput.innerHTML="";
+        printEmployees();
+    }
+}
+
+submit.onclick = addUserToArray;
+editDiv.onclick = interact;
+addDiv.onclick = interact;
+removeDiv.onclick = interact;
+xAdd.onclick = closeAddWindow;
+xEdit.onclick = closeEditWindow;
--- GyldenPizza/JS/modules/users.js
+++ GyldenPizza/JS/modules/users.js
@@ -1,20 +1,62 @@
 let Users = (function(){
     let usersArray = [
-        {id:"a00", name: "Sigve Berg Langva", email: "sigve", phone: "97890994", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "all", info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."},
-        {id:"a01", name: "Sanosh", email: "sanosh", phone: "23443549", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "drammen", info: "loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."},
-        {id:"a02", name: "Sondre", email: "sondre", phone: "97546848", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "alesund", info: "loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."},
-        {id:"a03", name: "Kamil", email: "kamil", phone: "12433453", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "oslo", info: "loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."},
-        {id:"a04", name: "Mathias", email: "mathias", phone: "12345678", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "oslo", info: "loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."},
-        {id:"a05", name: "Rolando", email: "rolando", phone: "98765432", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "oslo", info: "loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."},
-        {id:"a06", name: "Eivind", email: "eivind", phone: "55556666", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "oslo", info: "loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."} 
+        {id:0, name: "Sigve Berg Langva", email: "sigve", phone: "97890994", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "all", info: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."},
+        {id:1, name: "Sanosh", email: "sanosh", phone: "23443549", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "drammen", info: "loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."},
+        {id:2, name: "Sondre", email: "sondre", phone: "97546848", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "alesund", info: "loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."},
+        {id:3, name: "Kamil", email: "kamil", phone: "12433453", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "oslo", info: "loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."},
+        {id:4, name: "Mathias", email: "mathias", phone: "12345678", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "oslo", info: "loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."},
+        {id:5, name: "Rolando", email: "rolando", phone: "98765432", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "oslo", info: "loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."},
+        {id:6, name: "Eivind", email: "eivind", phone: "55556666", location: "norge", picture: "https://scontent.fosl4-2.fna.fbcdn.net/v/t1.18169-9/23130973_10154823178672687_2523489635196198363_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=QRSsTxJxGrIAX8BjxAt&_nc_ht=scontent.fosl4-2.fna&oh=d46efb00fa388c55b2eeff7830e88844&oe=60CD33EF", workplace: "oslo", info: "loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo quae est, deserunt nam sit nemo dolor, eos totam incidunt in aut recusandae ex itaque cupiditate nisi, expedita similique placeat."} 
     ];
+    let addUser = (input) =>{
+        usersArray.push(input);
+    }
+    let removeUser = (remove) =>{
+        for (let i = 0; i < usersArray.length; i++) {
+            if (usersArray[i].id == remove) {
+                usersArray.splice(i, 1);
+                break;
+            }
+        }
+    }
+    let editUser = (edit, item, change) =>{
+        for (let i = 0; i < usersArray.length; i++) {
+            if(usersArray[i].id == edit){
+                switch (item) {
+                    case "name":
+                        usersArray[i].name = change;
+                        break;
 
+                    case "email":
+                        usersArray[i].email = change;
+                        break;
+                    case "phone":
+                        usersArray[i].phone = change;
+
+                        break;
+                    case "picture":
+                        usersArray[i].picture = change;
+
+                        break;
+                    case "location":
+                        usersArray[i].location = change;
+                    
+                        break;
+                    case "workplace":
+                        usersArray[i].workplace = change;
+                        break;
+                    case "info":
+                        usersArray[i].info = change;
+                        break;
+                }
+            }
+        }
+    }
     const getAll = () => usersArray;
 
     const getById = ( id ) => {
         return usersArray.filter( userObject => userObject.id === id);
     }
-
     const length = ( usersArray.length );
 
     const getByName = ( name ) => {
@@ -29,6 +71,6 @@ let Users = (function(){
     const getByWorkplace = (workplace) => {
         return usersArray.filter(userObject => userObject.workplace === workplace);
     }
-    return {getAll, getById, length, getByName, getByEmail, getByPhone, getByWorkplace};
+    return {getAll, getById, length, getByName, getByEmail, getByPhone, getByWorkplace, addUser, removeUser, editUser};
  }());
  export default Users;
